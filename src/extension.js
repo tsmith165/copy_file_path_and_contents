43487e4f095e14e6f4896bc58ea994cc7cc27ec6
Torrey Smith <tas.smity@yahoo.com>
2023-10-16 08:41:59 -0700

    updated for version 0.0.4

--- src/extension.js
+++ src/extension.js
@@ -2,29 +2,36 @@ const vscode = require('vscode');
 const path = require('path');
 
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('extension.copyFilePathAndContent', async () => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) return; // No open text editor
+    let disposable = vscode.commands.registerCommand(
+        'extension.copyFilePathAndContent',
+        async () => {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) return; // No open text editor
 
-        const doc = editor.document;
-        
-        const workspaceFolders = vscode.workspace.workspaceFolders;
-        if (!workspaceFolders) return; // No workspace is opened
+            const doc = editor.document;
 
-        const workspaceRoot = workspaceFolders[0].uri.fsPath;
-        const relativePath = path.relative(workspaceRoot, doc.fileName);
-        const content = doc.getText();
+            const workspaceFolders = vscode.workspace.workspaceFolders;
+            if (!workspaceFolders) return; // No workspace is opened
 
-        const copyString = `
-/${relativePath}
+            const workspaceRoot = workspaceFolders[0].uri.fsPath;
+
+            const normalizedPath = path
+                .relative(workspaceRoot, doc.fileName)
+                .split(path.sep)
+                .join('/');
+            const trimmedContent = doc.getText().replace(/\s+$/g, ''); // removes trailing whitespace (including newlines)
+
+            const copyString = `
+/${normalizedPath}
 \`\`\`
-${content}
+${trimmedContent}
 \`\`\`
 `;
 
-        await vscode.env.clipboard.writeText(copyString);
-        vscode.window.showInformationMessage('File path and content copied to clipboard!');
-    });
+            await vscode.env.clipboard.writeText(copyString);
+            vscode.window.showInformationMessage('File path and content copied to clipboard!');
+        }
+    );
 
     context.subscriptions.push(disposable);
 }
@@ -33,5 +40,5 @@ function deactivate() {}
 
 module.exports = {
     activate,
-    deactivate
+    deactivate,
 };
